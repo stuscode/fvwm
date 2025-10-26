0a6c7b832efe755366db6c9f8f227c392bd20236
stew <stew@flybaker.com>
2025-10-26 10:34:58 -0400

    cleanup and minor changes

---
+++ launchterms.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+from tkinter import *
+from tkinter import filedialog
+
+DEFAULTPATH = "~/SyncDir/software"
+
+window = Tk()
+window.withdraw()
+if len(sys.argv) > 1:
+   path = sys.argv[1]
+else:
+   path = DEFAULTPATH
+startpath = os.path.expanduser(path)
+os.chdir(startpath)
+directory = filedialog.askdirectory()
+if len(directory) > 0:
+   os.chdir(directory) 
+   subprocess.Popen(['xterm', '-geometry', '80x79+0+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+494+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+988+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+1482+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+1976+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+2470+0'])
+   subprocess.Popen(['xterm', '-geometry', '80x79+2964+0'])

87834e23c9e951437ecfa71b099dd3f58b7cbb51
stew <stew@flybaker.com>
2026-01-25 18:09:10 -0500

    cleanup

--- scripts/loadEnv.py
+++ scripts/loadEnv.py
@@ -3,12 +3,6 @@
 import os
 import sys
 
-#with open('/home/stew/fvwmdebug','w') as d:
-#   print("here",file=d)
-#   print(sys.argv[0],file=d)
-#   print("here",file=d)
-#   print(sys.argv[1],file=d)
-#   print("here",file=d)
 with open(sys.argv[1], 'r') as f:
    for l in f:
       e,v = l.rstrip().split('=')

6b806f2eada99bdfb86388e57afa8509b3824ae5
sdbaker <sdbaker@work>
2026-07-14 11:15:28 -0400

    clean up and add options for resource name

--- scripts/launchterms.py
+++ scripts/launchterms.py
@@ -14,16 +14,17 @@ from tkinter import filedialog
 DEFAULTNUM = "3" #is a string for argument, gets converted same as
 
 p = argparse.ArgumentParser("launch group of xterms in specified directory")
-p.add_argument('-n', help="number of terminals to launch", default=DEFAULTNUM)
-p.add_argument('-i', help="initial directory for selector")
-p.add_argument('-xrm', help="x resource string for xterms. usually 'Page:desk p_off_x p_off_y'")
-p.add_argument('-d', help="start terminals in this directory, don't ask")
+p.add_argument("-n", "--numterms", help="number of terminals to launch", default=DEFAULTNUM)
+p.add_argument("-i", "--initialdir", help="initial directory for selector")
+p.add_argument("-xrm", "--xrmstring", help="x resource string for xterms. usually 'Page:desk p_off_x p_off_y'")
+p.add_argument("-r", "--resourcename", help="resource name")
+p.add_argument("-d", "--startingdir", help="start terminals in this directory, don't ask")
 args = p.parse_args()
-if args.d:
-   directory = os.path.expanduser(args.d)
+if args.startingdir:
+   directory = os.path.expanduser(args.startingdir)
 else: 
-   if args.i:
-      startdir = os.path.expanduser(args.i)
+   if args.initialdir:
+      startdir = os.path.expanduser(args.initialdir)
    else:
       startdir = os.path.expanduser('~')
    directory = filedialog.askdirectory(initialdir=startdir)
@@ -31,7 +32,14 @@ try: #if dir is empty, will start in cwd
    os.chdir(directory)
 except:
    print("Couldn't chdir to directory: ",directory)
-for i in range(int(args.n)):
+for i in range(int(args.numterms)):
    xpos = i * 494
    position = f"80x79+{xpos}+0"
-   subprocess.Popen(['xterm', '-geometry', position])
+   execargs = ['xterm', '-geometry', position]
+   if args.xrmstring != None:
+      execargs.append('-xrm')
+      execargs.append(args.xrmstring)
+   if args.resourcename != None:
+      execargs.append('-name')
+      execargs.append(args.resourcename)
+   subprocess.Popen(execargs)

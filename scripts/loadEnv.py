1dcf756cd36a9214555d5e83930deff4896189b8
stew <stew@flybaker.com>
2026-07-22 13:24:00 -0400

    update

--- scripts/loadEnv.py
+++ scripts/loadEnv.py
@@ -7,4 +7,7 @@ with open(sys.argv[1], 'r') as f:
    for l in f:
       e,v = l.rstrip().split('=')
       print('setEnv', e, v)
+      key,winid = e.split('-')
+      print('Echo exec BrowserUseName', winid)
+      print('Function BrowserUseName', winid)
 #      print('InfoStoreAdd', e, v)
